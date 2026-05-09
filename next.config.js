64424d068eaf1116dc7842485f2312b76a635961
myskix <iksymm01@gmail.com>
2026-05-09 15:24:32 +0700

    chore: upgrade depedencies

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swMinify: true,
   disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
 });
 
 const nextConfig = {
